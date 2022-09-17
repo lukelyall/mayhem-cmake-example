1c0d0cc828ee2decfed16c08ae03411b20ab332e
lukelyall <lukelyall2012@gmail.com>
2022-09-17 15:08:18 -0400

    convert to libFuzzer target

--- fuzzme.c
+++ fuzzme.c
@@ -17,6 +17,7 @@ int fuzzme(char *buf)
 
 #define BUFSZ 256
 
+/*
 int main(int argc, char** argv)
 {
     char buf[BUFSZ] = { 0 };
@@ -41,4 +42,8 @@ int main(int argc, char** argv)
 
     return 0;
 }
+*/
 
+int  LLVMFuzzerTestOneInput(char* data, size_t size){
+	return fuzzme(data);
+}
